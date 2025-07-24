9598a94cf4fc109b7b5631d50e8380376cc5f3fd
Pouya Mahdavi <pmahdavi@cpex.com>
2025-07-24 16:50:24 -0400

    add test

--- src/main.py
+++ src/main.py
@@ -3,3 +3,11 @@ from utils import add
 print(add(a, b))   
 print("Hello, World!")
 print("This is a test script.")
+
+
+def test():
+    """Test function to demonstrate functionality."""
+    a = 5
+    b = 3
+    result = add(a, b)
+    print(f"The result of adding {a} and {b} is: {result}")
