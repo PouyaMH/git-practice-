848576b04ff2ee63a44c2eb3eed1587e50ecdc65
Pouya Mahdavi <pmahdavi@cpex.com>
2025-07-25 13:43:34 -0400

    add

--- src/main.py
+++ src/main.py
@@ -7,3 +7,10 @@ print (" seoration ")
 
 
 "let see iof thisndivergen between iss54 and main work"
+
+
+def main():
+    a = 5
+    b = 10
+    result = add(a, b)
+    print(f"The result of adding {a} and {b} is: {result}")
